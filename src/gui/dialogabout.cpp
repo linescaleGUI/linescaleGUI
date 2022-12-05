c2c231f3b0f28e0b91f2322b514600321ee4fcb0
Marco Niederberger <marco@ni-m.ch>
2022-12-05 19:29:58 +0100

    Add dialogAbout
    #18

---
+++ src/gui/dialogabout.cpp
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Copyright (C) 2022 by Gschwind, Weber, Schoch, Niederberger                *
+ *                                                                            *
+ * This file is part of linescaleGUI.                                         *
+ *                                                                            *
+ * LinescaleGUI is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * LinescaleGUI is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with linescaleGUI. If not, see <http://www.gnu.org/licenses/>.       *
+ ******************************************************************************/
+/**
+ * @file dialogabout.cpp
+ * @authors Gschwind, Weber, Schoch, Niederberger
+ *
+ */
+
+#include "dialogabout.h"
+#include "version.h"
+
+DialogAbout::DialogAbout(QWidget* parent) : QDialog(parent), ui(new Ui::DialogAbout) {
+    ui->setupUi(this);
+    replaceVersionInfo();
+}
+
+DialogAbout::~DialogAbout() {
+    delete ui;
+}
+
+void DialogAbout::replaceVersionInfo() {
+    QString currentText = ui->lblAbout->text();
+
+    static_assert(sizeof(version::gitRev) >= 8);
+    QString rev = QString::fromUtf8(version::gitRev, 8);
+
+    currentText.replace("#GitHash", rev)
+        .replace("#compDate", version::buildDate)
+        .replace("#compTime", version::buildTime)
+        .replace("#GitLink", version::repoUrl);
+    ui->lblAbout->setText(currentText);
+}
---
+++ src/gui/dialogabout.h
@@ -0,0 +1,54 @@
+/******************************************************************************
+ * Copyright (C) 2022 by Gschwind, Weber, Schoch, Niederberger                *
+ *                                                                            *
+ * This file is part of linescaleGUI.                                         *
+ *                                                                            *
+ * LinescaleGUI is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * LinescaleGUI is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with linescaleGUI. If not, see <http://www.gnu.org/licenses/>.       *
+ ******************************************************************************/
+/**
+ * @file dialogabout.h
+ * @authors Gschwind, Weber, Schoch, Niederberger
+ *
+ * @brief Dialog to display version info
+ *
+ */
+
+#pragma once
+#ifndef DIALOGABOUT_H_
+#define DIALOGABOUT_H_
+
+#include <QDialog>
+#include "ui_dialogabout.h"
+
+namespace Ui {
+class DialogAbout;
+}
+
+class DialogAbout : public QDialog {
+    Q_OBJECT
+
+   public:
+    DialogAbout(QWidget* parent = nullptr);
+    ~DialogAbout();
+
+   private:
+    /** @brief Replace version placeholder in ui file with actual data
+     * generated on compile time.
+     * This contains github url, commit hash and compile time.
+     */
+    void replaceVersionInfo(void);
+    Ui::DialogAbout* ui;
+};
+
+#endif  // DIALOGABOUT_H_
