be96c81f7173bb6ef3cfe7e2a0f0a2f4f1faef03
ni-m <ni-m@users.noreply.github.com>
2022-12-21 15:39:39 +0000

    deploy: cc5d0dec22bd7634862019e18911777daf514d1a

--- doxygen/search/all_c.js
+++ doxygen/search/all_c.js
@@ -2,10 +2,9 @@ var searchData=
 [
   ['newsampledevice_0',['newSampleDevice',['../classcomm_1_1_comm_device.html#a48d6a41c66514a5179f1d3c4eacb446f',1,'comm::CommDevice']]],
   ['newsamplemaster_1',['newSampleMaster',['../classcomm_1_1_comm_master.html#ac79445d43a6c23ef288dbd2911ebbf97',1,'comm::CommMaster']]],
-  ['notification_2',['Notification',['../class_notification.html',1,'']]],
+  ['notification_2',['Notification',['../class_notification.html',1,'Notification'],['../class_notification.html#a7e13ad8e77c26c7f8107e1ea0751e005',1,'Notification::Notification()']]],
   ['notification_3',['notification',['../class_main_window.html#a0a49898057d81d4444ae46e9215fc1e1',1,'MainWindow']]],
-  ['notification_4',['Notification',['../class_notification.html#a7e13ad8e77c26c7f8107e1ea0751e005',1,'Notification']]],
-  ['notification_2ecpp_5',['notification.cpp',['../notification_8cpp.html',1,'']]],
-  ['notification_2eh_6',['notification.h',['../notification_8h.html',1,'']]],
-  ['notification_5fh_5f_7',['NOTIFICATION_H_',['../notification_8h.html#a78aa9a6ce6a6d2916e745e0cc7416f95',1,'notification.h']]]
+  ['notification_2ecpp_4',['notification.cpp',['../notification_8cpp.html',1,'']]],
+  ['notification_2eh_5',['notification.h',['../notification_8h.html',1,'']]],
+  ['notification_5fh_5f_6',['NOTIFICATION_H_',['../notification_8h.html#a78aa9a6ce6a6d2916e745e0cc7416f95',1,'notification.h']]]
 ];
--- doxygen/search/functions_c.js
+++ doxygen/search/functions_c.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['selectionchanged_0',['selectionChanged',['../class_plot.html#ae3ce9847c3c33a241d3d1961396f5e7c',1,'Plot']]],
-  ['senddata_1',['sendData',['../classcomm_1_1_comm_device.html#ad7ffdfeaeafb5a09294c83c4a3cff929',1,'comm::CommDevice::sendData()'],['../classcomm_1_1_comm_master.html#a8605d1bc19aed2046b6b4593ac1b361c',1,'comm::CommMaster::sendData(const QByteArray &amp;rawData)'],['../classcomm_1_1_comm_master.html#a7ce73489c87b14a0781b2e07f87a5d7b',1,'comm::CommMaster::sendData(const QString &amp;rawData)'],['../classcomm_1_1_comm_u_s_b.html#a3af3c8634126ff8580a891da5ffd465d',1,'comm::CommUSB::sendData()']]],
-  ['sendmsg_2',['sendMsg',['../class_dialog_debug.html#aaf8cb281c2472853996a680b59bb42f2',1,'DialogDebug']]],
-  ['sendresetpeak_3',['sendResetPeak',['../class_main_window.html#a564851c2b756c24b98c54866aa3500ea',1,'MainWindow']]],
-  ['sendsetabsolutezero_4',['sendSetAbsoluteZero',['../class_main_window.html#a8f9da4488071d9fc2a9144c7200b2234',1,'MainWindow']]],
-  ['sendsetrelativezero_5',['sendSetRelativeZero',['../class_main_window.html#a22920462729125179f363c2eb11b0fe2',1,'MainWindow']]],
-  ['setcommunicationmaster_6',['setCommunicationMaster',['../class_connection_widget.html#ad0980ccc72b04786d7553cae02c7dadb',1,'ConnectionWidget']]],
-  ['setfreq_7',['setFreq',['../classcomm_1_1_comm_device.html#a4bebce0d23c54f80f705221e9175b5ad',1,'comm::CommDevice']]],
-  ['setnewfreq_8',['setNewFreq',['../classcomm_1_1_comm_master.html#a3ff7ddcdaf0f923be560692266d3580f',1,'comm::CommMaster']]],
-  ['showevent_9',['showEvent',['../class_dialog_connect.html#a837300a8690091004b49817fd9778be8',1,'DialogConnect']]],
-  ['showlog_10',['showLog',['../class_main_window.html#abcc6ca124ea45bfd45bf0d484837343c',1,'MainWindow']]]
+  ['savelog_0',['saveLog',['../class_notification.html#a55ba3de37658a449ffc4687e7f984776',1,'Notification']]],
+  ['selectionchanged_1',['selectionChanged',['../class_plot.html#ae3ce9847c3c33a241d3d1961396f5e7c',1,'Plot']]],
+  ['senddata_2',['sendData',['../classcomm_1_1_comm_device.html#ad7ffdfeaeafb5a09294c83c4a3cff929',1,'comm::CommDevice::sendData()'],['../classcomm_1_1_comm_master.html#a8605d1bc19aed2046b6b4593ac1b361c',1,'comm::CommMaster::sendData(const QByteArray &amp;rawData)'],['../classcomm_1_1_comm_master.html#a7ce73489c87b14a0781b2e07f87a5d7b',1,'comm::CommMaster::sendData(const QString &amp;rawData)'],['../classcomm_1_1_comm_u_s_b.html#a3af3c8634126ff8580a891da5ffd465d',1,'comm::CommUSB::sendData()']]],
+  ['sendmsg_3',['sendMsg',['../class_dialog_debug.html#aaf8cb281c2472853996a680b59bb42f2',1,'DialogDebug']]],
+  ['sendresetpeak_4',['sendResetPeak',['../class_main_window.html#a564851c2b756c24b98c54866aa3500ea',1,'MainWindow']]],
+  ['sendsetabsolutezero_5',['sendSetAbsoluteZero',['../class_main_window.html#a8f9da4488071d9fc2a9144c7200b2234',1,'MainWindow']]],
+  ['sendsetrelativezero_6',['sendSetRelativeZero',['../class_main_window.html#a22920462729125179f363c2eb11b0fe2',1,'MainWindow']]],
+  ['setcommunicationmaster_7',['setCommunicationMaster',['../class_connection_widget.html#ad0980ccc72b04786d7553cae02c7dadb',1,'ConnectionWidget']]],
+  ['setfreq_8',['setFreq',['../classcomm_1_1_comm_device.html#a4bebce0d23c54f80f705221e9175b5ad',1,'comm::CommDevice']]],
+  ['setnewfreq_9',['setNewFreq',['../classcomm_1_1_comm_master.html#a3ff7ddcdaf0f923be560692266d3580f',1,'comm::CommMaster']]],
+  ['showevent_10',['showEvent',['../class_dialog_connect.html#a837300a8690091004b49817fd9778be8',1,'DialogConnect']]],
+  ['showlog_11',['showLog',['../class_main_window.html#abcc6ca124ea45bfd45bf0d484837343c',1,'MainWindow']]]
 ];
